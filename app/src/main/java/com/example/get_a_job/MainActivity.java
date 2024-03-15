4e2162c9138a654b42c8eb194c2ec64a99d24f70
aksh0010 <akshpatelofficial@gmail>
2024-03-15 15:16:49 -0400

    login page with shared interfaces

--- app/src/main/java/com/example/get_a_job/MainActivity.java
+++ app/src/main/java/com/example/get_a_job/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.get_a_job;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText et_username, et_password;
+    Button btn_submit;
+    Switch btn_switch;
+    SharedPreferences sharedPreferences ;
+    SharedPreferences.Editor sp_editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_username = findViewById(R.id.et_username);
+        et_password=findViewById(R.id.et_password);
+        btn_submit=findViewById(R.id.btn_submit);
+        btn_switch = findViewById(R.id.btn_switch);
+        get_userData();
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(btn_switch.isChecked()){
+                    saveUserData();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        if(btn_switch.isChecked()){
+            saveUserData();
+        }
+        super.onPause();
+    }
+    public  void saveUserData(){
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        sp_editor = sharedPreferences.edit();
+
+        sp_editor.putString("key_username",et_username.getText().toString());
+        sp_editor.putString("key_password",et_password.getText().toString());
+        sp_editor.putBoolean("key_switch",btn_switch.isChecked());
+        sp_editor.commit();
+
+    }
+
+    public void get_userData(){
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        et_username.setText(sharedPreferences.getString("key_username",null));
+        et_password.setText(sharedPreferences.getString("key_password",null));
+        btn_switch.setChecked(sharedPreferences.getBoolean("key_switch",false));
+
     }
 }
